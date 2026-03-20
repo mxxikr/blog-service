26f849e5c724338250d57c99b29fcd2b91a03107
mxxikr <mxxikr@gmail.com>
2026-03-20 18:17:43 +0900

    feat: 레포지토리 인터페이스 작성

---
+++ src/main/java/com/blogService/repository/BlogRepository.java
@@ -0,0 +1,7 @@
+package com.blogService.repository;
+
+import com.blogService.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlogRepository extends JpaRepository<Article, Long> {
+}
