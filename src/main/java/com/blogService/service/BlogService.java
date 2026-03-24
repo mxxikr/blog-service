fc0a09399cc026c92240880a3dec864fea381093
mxxikr <mxxikr@gmail.com>
2026-03-24 16:23:24 +0900

    feat: 글 수정, 삭제, 글쓴이 확인 로직 추가

--- src/main/java/com/blogService/service/BlogService.java
+++ src/main/java/com/blogService/service/BlogService.java
@@ -5,6 +5,7 @@ import com.blogService.dto.AddArticleRequest;
 import com.blogService.dto.UpdateArticleRequest;
 import com.blogService.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -42,6 +43,8 @@ public class BlogService {
      * 블로그 글 삭제
      */
     public void delete(Long id) {
+        Article article = blogRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+        authorizeArticleAuthor(article);
         blogRepository.deleteById(id);
     }
 
@@ -52,8 +55,18 @@ public class BlogService {
     public Article update(long id, UpdateArticleRequest request) {
         Article article = blogRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("not found: " + id));
 
+        authorizeArticleAuthor(article);
         article.update(request.getTitle(), request.getContent());
 
         return article;
+    }
+
+    // 게시글을 작성한 유저인지 확인
+    private static void authorizeArticleAuthor(Article article) {
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        if (!article.getAuthor().equals(userName)) {
+            throw new IllegalArgumentException("not authorized");
+        }
     }
 }
